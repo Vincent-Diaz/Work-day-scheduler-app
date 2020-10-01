8977a4e13a57634b3b0993db3c55e4f3cfd280b6
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-09-30 21:29:36 -0500

    added saveBtn .on click and added updateHour func.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+    // Listen for save button clicks
+    $(".saveBtb").on("click", function (){
+        //nearby values
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Set in local storage
+        localStorage.setItem(time, value);
+    });
+
+    function updateHour() {
+        // Current number of hours
+        var currentHour = moment().hours();
+        console.log(currentHour);
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var rowHour = parseInt($(this).attr("id"));
+            console.log(rowHour);
+        })
+    }
+
+    updateHour();
 })
