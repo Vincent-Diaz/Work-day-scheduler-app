a42085ecfb857a6dae550faf59a363f32a8bf5b4
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-09-30 23:20:09 -0500

    added get item from local storage to render data

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 
     // Listen for save button clicks
-    $(".saveBtb").on("click", function (){
+    $(".saveBtn").on("click", function (){
         //nearby values
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -14,6 +14,17 @@ $(document).ready(function() {
         // Set in local storage
         localStorage.setItem(time, value);
     });
+
+    // Load saved data from localStorage
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
     function updateHour() {
         // Current number of hours
